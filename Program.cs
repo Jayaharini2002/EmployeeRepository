02023e36a3c38f989cf06f411ca7b9974aaeb3fb
Jayaharini <svhg2002@gmail.com>
2024-02-13 10:53:15 +0530

    change5

--- Program.cs
+++ Program.cs
@@ -1,33 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using ListEmployees1.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Serilog;
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddDbContext<ListEmployees1Context>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("ListEmployees1Context") ?? throw new InvalidOperationException("Connection string 'ListEmployees1Context' not found.")));
+
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
         builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
         .AddCookie(option =>
-         {
-             option.LoginPath = "/Home/Index";
-             option.ExpireTimeSpan= TimeSpan.FromMinutes(20);
-         });
+        {
+            option.LoginPath = "/Home/Index";
+            option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        });
+       
+
         builder.Services.AddAuthorization();
         builder.Services.AddRazorPages();
+       
+        builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
         var app = builder.Build();
-
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
+        }
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
         }
 
         app.UseHttpsRedirection();
